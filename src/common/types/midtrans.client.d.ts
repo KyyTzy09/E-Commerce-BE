3e07e9fb71e9568e37c9ef8fb8fb144129101ba3
Firegame224 <xg474627@gmail.com>
2025-07-04 13:07:44 +0700

    fix: fix type error

--- src/common/types/midtrans.client.d.ts
+++ src/common/types/midtrans.client.d.ts
@@ -1,18 +1,22 @@
 declare module "midtrans-client" {
-    export class Snap {
-        constructor(params: {
-            isProduction: boolean
-            serverKey: string
-            clientKey: string
-        })
-        createTransaction(params: any): Promise<any>
-    }
-    export class coreApi {
-        constructor(params: {
-            isProduction: Boolean
-            serverKey: String
-            clientKey: string
-        })
-        charge(params: any): Promise<any>
+    export default midtransClient
+
+    namespace midtransClient {
+        class Snap {
+            constructor(params: {
+                isProduction: boolean
+                serverKey: string
+                clientKey: string
+            })
+            createTransaction(params: any): Promise<any>
+        }
+        class coreApi {
+            constructor(params: {
+                isProduction: boolean
+                serverKey: string
+                clientKey: string
+            })
+            charge(params: any): Promise<any>
+        }
     }
 }
